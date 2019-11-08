d29d0ebca8d944bfa1cb594d5e89d37422cc58b2
heyrict <xiezh0831@yahoo.co.jp>
2019-11-08 18:25:23 +0800

    Implements Formatter::into_elements

--- src/lib.rs
+++ src/lib.rs
@@ -99,18 +99,23 @@ impl<'a> Formatter<'a> {
             .join("")
     }
 
+    // TODO: This function is rather messed, it works, but is pretty hard to maintain.
     fn into_elements(&self) -> Vec<Element> {
         if self.text.len() < 2 {
             return vec![Element::Text(self.text.to_owned())];
         }
-        // TODO Implement this
-        let mut prev_char: char = ' ';
+        let mut prev_char: Option<char> = None;
         let mut context: Option<Wrapper> = None;
         let mut new_context: Option<Wrapper> = None;
         let mut elements: Vec<Element> = vec![];
         let mut current_texts = String::new();
 
         for c in self.text.chars() {
+            if prev_char.is_none() {
+                prev_char = Some(c);
+                continue;
+            }
+
             // When it is not inside a wrapper currently
             if context.is_none() {
                 // Check if current char(s) matches a prefix.
@@ -118,11 +123,14 @@ impl<'a> Formatter<'a> {
                 for w in Wrapper::values().into_iter() {
                     let mut wrapper_chars = w.get_prefix().chars();
                     let is_match = match w.get_prefix().len() {
-                        1 => wrapper_chars.nth(0).unwrap() == c,
+                        1 => {
+                            let first = wrapper_chars.next();
+                            first == prev_char
+                        }
                         2 => {
                             let first = wrapper_chars.next();
                             let second = wrapper_chars.next();
-                            first == Some(prev_char) && second == Some(c)
+                            first == prev_char && second == Some(c)
                         }
                         _ => false,
                     };
@@ -133,46 +141,85 @@ impl<'a> Formatter<'a> {
                     }
                 }
 
-                if new_context.is_some() && current_texts.len() > 0 {
-                    elements.push(Element::Text(current_texts.clone()));
-                    current_texts = String::new();
-                } else {
-                    // Add char to text buffer;
-                    current_texts.push(prev_char);
-                }
+                // If wrapped, create a text element
+                if let Some(wrapper) = &new_context {
+                    if current_texts.len() > 0 {
+                        elements.push(Element::Text(current_texts.clone()));
+                        current_texts = String::new();
+                    }
+                    // Update prev_char
+                    if wrapper.get_prefix().len() == 1 {
+                        prev_char = Some(c);
+                    } else {
+                        prev_char = None;
+                    }
+                // Update current_texts
+                } else if let Some(pc) = prev_char {
+                    current_texts.push(pc);
+                    prev_char = Some(c);
+                };
+            // When it is inside a wrapper currently
             } else {
+                // Check if current char(s) matches the suffix.
                 let wrapper = context.clone().unwrap();
-                let prefix_len = wrapper.get_prefix().len();
                 let suffix_len = wrapper.get_suffix().len();
                 let mut wrapper_chars = wrapper.get_suffix().chars();
-                let is_match = match wrapper.get_suffix().len() {
-                    1 => wrapper_chars.nth(0).unwrap() == c,
+                let is_match = match suffix_len {
+                    1 => wrapper_chars.next() == prev_char,
                     2 => {
                         let first = wrapper_chars.next();
                         let second = wrapper_chars.next();
-                        first == Some(prev_char) && second == Some(c)
+                        first == prev_char && second == Some(c)
                     }
                     _ => false,
                 };
 
+                // If so, update the new_context.
                 if is_match {
-                    current_texts.push(c);
                     new_context = None;
-                    let text_len = current_texts.len();
-                    dbg!(&text_len, &current_texts, &suffix_len);
                     elements.push(Element::Wrapped(Item {
                         wrapper: wrapper.clone(),
-                        text: current_texts
-                            .get(prefix_len..(text_len - suffix_len))
-                            .unwrap()
-                            .to_owned(),
+                        text: current_texts.to_owned(),
                     }));
-                    current_texts = String::new();
-                }
-            }
 
+                    // Update current_texts
+                    current_texts = String::new();
+                    if suffix_len == 1 {
+                        prev_char = Some(c);
+                    } else {
+                        prev_char = None;
+                    }
+                } else if let Some(pc) = prev_char {
+                    current_texts.push(pc);
+                    prev_char = Some(c);
+                };
+            }
             context = new_context.clone();
-            prev_char = c;
+        }
+
+        // Last char
+        if context.is_none() {
+            if current_texts.len() > 0 {
+                current_texts.push(prev_char.unwrap());
+                elements.push(Element::Text(current_texts));
+            };
+        } else {
+            let first_char = context.clone().unwrap().get_suffix().chars().next();
+            let is_match = first_char == prev_char;
+
+            if is_match {
+                elements.push(Element::Wrapped(Item {
+                    wrapper: context.unwrap(),
+                    text: current_texts,
+                }));
+            } else {
+                let mut text = String::from(context.unwrap().get_prefix());
+                if let Some(pc) = prev_char {
+                    current_texts.push(pc);
+                }
+                text.push_str(&current_texts);
+                elements.push(Element::Text(text));
+            }
         }
 
         elements
@@ -184,7 +231,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_general() {
+    fn parse_curly() {
         let formatter = Formatter::new("Hello, {name}");
         assert_eq!(
             formatter.into_elements(),
@@ -193,6 +240,85 @@ mod tests {
                 Element::Wrapped(Item {
                     wrapper: Wrapper::Curly,
                     text: "name".to_owned()
+                })
+            ]
+        );
+    }
+
+    #[test]
+    fn parse_double_curly() {
+        let formatter = Formatter::new("Hello, {{ user.name }}");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![
+                Element::Text("Hello, ".to_owned()),
+                Element::Wrapped(Item {
+                    wrapper: Wrapper::DoubleCurly,
+                    text: " user.name ".to_owned()
+                })
+            ]
+        );
+    }
+
+    #[test]
+    fn parse_dollar_curly() {
+        let formatter = Formatter::new("A ${plus} B");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![
+                Element::Text("A ".to_owned()),
+                Element::Wrapped(Item {
+                    wrapper: Wrapper::DollarCurly,
+                    text: "plus".to_owned(),
+                }),
+                Element::Text(" B".to_owned()),
+            ]
+        );
+    }
+
+    #[test]
+    fn parse_curly_hash() {
+        let formatter = Formatter::new("{##}");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![Element::Wrapped(Item {
+                wrapper: Wrapper::CurlyHash,
+                text: String::new(),
+            })]
+        );
+
+        let formatter = Formatter::new("{#}");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![Element::Text("{#}".to_owned()),]
+        );
+
+        let formatter = Formatter::new("{# comment #}");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![Element::Wrapped(Item {
+                wrapper: Wrapper::CurlyHash,
+                text: " comment ".to_owned(),
+            })]
+        );
+    }
+
+    #[test]
+    fn parse_curly_percent() {
+        let formatter = Formatter::new("{%}");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![Element::Text("{%}".to_owned()),]
+        );
+
+        let formatter = Formatter::new("Hello, {% name %}");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![
+                Element::Text("Hello, ".to_owned()),
+                Element::Wrapped(Item {
+                    wrapper: Wrapper::CurlyPercent,
+                    text: " name ".to_owned()
                 })
             ]
         );
@@ -208,21 +334,29 @@ mod tests {
                 text: "n".to_owned()
             })]
         );
+
+        let formatter = Formatter::new("{}");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![Element::Wrapped(Item {
+                wrapper: Wrapper::Curly,
+                text: String::new(),
+            })]
+        );
     }
 
     #[test]
-    fn parse_empty() {
-        let formatter = Formatter::new("A ${} B");
+    fn parse_broken() {
+        let formatter = Formatter::new("${");
         assert_eq!(
             formatter.into_elements(),
-            vec![
-                Element::Text("A ".to_owned()),
-                Element::Wrapped(Item {
-                    wrapper: Wrapper::DollarCurly,
-                    text: String::new()
-                }),
-                Element::Text(" B".to_owned()),
-            ]
+            vec![Element::Text("${".to_owned())]
+        );
+
+        let formatter = Formatter::new("${todo..");
+        assert_eq!(
+            formatter.into_elements(),
+            vec![Element::Text("${todo..".to_owned())]
         );
     }
 }
